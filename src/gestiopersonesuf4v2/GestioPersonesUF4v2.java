d6c3f6ba4d0ebf8461a7b24c6c406e984f68cbb8
David Vilanova <david.vilanova@cirvianum.cat>
2024-06-13 10:07:45 +0200

    prova config

--- src/gestiopersonesuf4v2/GestioPersonesUF4v2.java
+++ src/gestiopersonesuf4v2/GestioPersonesUF4v2.java
@@ -2,7 +2,7 @@ package gestiopersonesuf4v2;
 
 import controller.FrontController;
 
-public class GestioPersonesUF4v2 {
+public class GestioPersonesUF4v2 {//Hola
     public static void main(String[] args) throws Exception {
         GestioPersonesUF4v2 app = new GestioPersonesUF4v2();
         app.ini();
